9112103e91747371efd382b8442507f807c994c3
jisoo-lee-furiosaAI <jisoo.lee@furiosa.ai>
2026-02-06 13:31:06 +0900

    [mod] modify benchmark option for bge-m3

--- evaluation/run_mteb_by_task.sh
+++ evaluation/run_mteb_by_task.sh
@@ -14,8 +14,6 @@ python run_mteb.py \
   --model_name ${model_name} \
   --backend "openai" \
   --precision bf16 \
-  --model_kwargs "{\"max_length\": 8192, \"attn_type\": \"causal\", \"pooler_type\": \"last\", \"do_norm\": true, \"use_instruction\": true, \"instruction_template\": \"Instruct: {}\nQuery:\", \"instruction_dict_path\": \"task_prompts.json\", \"attn_implementation\":\"flash_attention_2\"}" \
-  --run_kwargs "{\"save_predictions\": \"true\"}" \
   --output_dir results/${model_name} \
-  --batch_size 2 \
+  --batch_size 1 \
   --tasks "${tasks}"
